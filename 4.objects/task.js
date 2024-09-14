faa3f2fe12f3d837d3281f34a1ec18c7e4a2f4e0
Olik <prikhodkolik15@gmail.com>
2024-09-14 18:04:36 +0500

    Решение

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,34 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;   
 }
+   let student1 = new Student("Василиса", "женский", 19);
+   let student2 = new Student("Артём", "мужской", 25); 
+
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if(this.marks === undefined){
+        this.marks = (marks);   
+    } else {
+        this.marks = this.marks.concat(marks);
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if(this.marks === undefined){
+        return 0;
+    } else {
+        return this.marks.reduce((acc, item) => acc + item, 0) / this.marks.length;
+    }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    this.excluded = reason;
+    delete this.subject;
+    delete this.marks;
 }
