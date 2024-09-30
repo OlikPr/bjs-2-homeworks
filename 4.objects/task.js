f3891168aa72b7939a56a4afc52059dae9db26c5
Olik <prikhodkolik15@gmail.com>
2024-09-30 20:31:47 +0500

    Решение

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,34 +1,32 @@
 function Student(name, gender, age) {
     this.name = name;
     this.gender = gender;
-    this.age = age;   
-}
-   let student1 = new Student("Василиса", "женский", 19);
-   let student2 = new Student("Артём", "мужской", 25); 
-
-
-Student.prototype.setSubject = function (subjectName) {
-    this.subject = subjectName;
-}
-
-Student.prototype.addMarks = function (...marks) {
-    if(this.marks === undefined){
-        this.marks = (marks);   
+    this.age = age;
+    this.marks = [];
+  }
+  
+  Student.prototype.setSubject = function(subjectName) {
+    this.subject = subjectName
+  }
+  
+  Student.prototype.addMarks = function(...marksToAdd) {
+    if (this.marks === undefined) {
+      return
     } else {
-        this.marks = this.marks.concat(marks);
+      this.marks.push(...marksToAdd)
     }
-}
-
-Student.prototype.getAverage = function () {
-    if(this.marks === undefined){
-        return 0;
+  }
+  
+  Student.prototype.getAverage = function() {
+    if (this.marks === undefined || !this.marks.length) {
+      return 0
     } else {
-        return this.marks.reduce((acc, item) => acc + item, 0) / this.marks.length;
+      return this.marks.reduce((sum, current) => sum + current, 0) / this.marks.length
     }
-}
-
-Student.prototype.exclude = function (reason) {
-    this.excluded = reason;
-    delete this.subject;
-    delete this.marks;
-}
+  }
+  
+  Student.prototype.exclude = function(reason) {
+    this.excluded = reason
+    delete this.subject
+    delete this.marks
+  }
